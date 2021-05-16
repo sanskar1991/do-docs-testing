6350993b80022a787009c03ec04a72f8e633d6a6
sanskar1991 <sanskarj123@gmail.com>
2021-05-16 14:25:41 +0530

    Added initial tests

---
+++ dodocs/tests/customers/test_create_customers_smoke.py
@@ -0,0 +1,8 @@
+import pytest
+import logging as logger
+
+
+@pytest.mark.test29
+def test_create_customer_only_email_password():
+
+    logger.info("TEST: Create customer's only email and password")
---
+++ dodocs/tests/test_healthcheck.py
@@ -0,0 +1,5 @@
+import logging as logger
+
+
+def test_healthcheck_01():
+    logger.info("Just running healthcheck")
