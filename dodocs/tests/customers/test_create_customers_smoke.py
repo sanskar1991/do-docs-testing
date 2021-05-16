585c7b189006f52cc16f4a6b845ef7610f992ef9
sanskar1991 <sanskarj123@gmail.com>
2021-05-16 15:06:49 +0530

    Added utility to generate random email and password

--- dodocs/tests/customers/test_create_customers_smoke.py
+++ dodocs/tests/customers/test_create_customers_smoke.py
@@ -1,16 +1,20 @@
 import pytest
 import logging as logger
 
+from dodocs.src.utilities.generic_utility import generate_unique_email_password
 
 @pytest.mark.test29
 def test_create_customer_only_email_password():
 
     logger.info("TEST: Create customer's only email and password")
     # email should be unique for every call
-    email = ''
-    password = ''
+
+    rand_info = generate_unique_email_password()
+    email = rand_info['email']
+    password = rand_info['password']
 
     # create payload
+    payload = {'email': email, 'password': password}
 
     # make the api call
 
