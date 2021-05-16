8ed7a1e0b0dedd1f169b6c50478e5ad1972f9ce3
sanskar1991 <sanskarj123@gmail.com>
2021-05-16 14:24:12 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dodocs",
+    description="",
+    version="1.0",
+    author="Sanskar Jain",
+    author_email="sanskarj123@gmail.com",
+    # url="",
+    packages=find_packages()
+)
