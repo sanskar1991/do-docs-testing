fa401e10d303d72d539a094edc47b7ea3b9f58d2
sanskar1991 <sanskarj123@gmail.com>
2021-05-16 14:32:08 +0530

    Added pseudo code for test

--- dodocs/tests/customers/test_create_customers_smoke.py
+++ dodocs/tests/customers/test_create_customers_smoke.py
@@ -6,3 +6,16 @@ import logging as logger
 def test_create_customer_only_email_password():
 
     logger.info("TEST: Create customer's only email and password")
+    # email should be unique for every call
+    email = ''
+    password = ''
+
+    # create payload
+
+    # make the api call
+
+    # verify status code of the call
+
+    # verify email in the response
+
+    # verify customer is created in the database
